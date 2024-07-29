eaf92bb60c5080651f21287271426660bbd9b5db
rendypratama01 <m.rendy5pratama@gmail.com>
2024-07-29 22:37:43 +0700

    tambah library

--- src/App.js
+++ src/App.js
@@ -1,29 +1,31 @@
 import "../src/styles/style.css";
 import { Route, Routes, useLocation } from "react-router-dom";
-import SideBar from "./components/SideBar";
+import Sidebar from "./components/Sidebar";
 import DashboardPage from "./pages/DashboardPage";
 import DetailMisiPage from "./pages/DetailMisiPage";
 import DetailOperatorPage from "./pages/DetailOperatorPage";
 import DetailWahanaPage from "./pages/DetailWahanaPage";
-import DetailMaintenancePage from "./pages/DetailMaintenancePage";
+import DetailMaintenanceWahanaPage from "./pages/DetailMaintenanceWahanaPage";
+import DetailMaintenanceKomponenPage from "./pages/DetailMaintenanceKomponenPage";
 import DetailKomponenPage from "./pages/DetailKomponenPage";
 import MisiPage from "./pages/MisiPage";
 import OperatorPage from "./pages/OperatorPage";
 import WahanaPage from "./pages/WahanaPage";
-import MaintenancePage from "./pages/MaintenancePage";
+import MaintenanceWahanaPage from "./pages/MaintenanceWahanaPage";
+import MaintenanceKomponenPage from "./pages/MaintenanceKomponenPage";
 import KomponenPage from "./pages/KomponenPage";
 import LoginPage from "./pages/LoginPage";
 import ProfilePage from "./pages/ProfilePage";
 import ProtectedRoute from "./components/ProtectedRoute"; // Import the ProtectedRoute component
-import { rootPath, dashboardPath, misiPath, operatorPath, wahanaPath, maintenancePath, komponenPath, detailMisiPath, detailOperatorPath, detailWahanaPath, detailMaintenancePath, detailKomponenPath, loginPath, profilePath } from "./routes";
+import { rootPath, dashboardPath, misiPath, operatorPath, wahanaPath, maintenanceWahanaPath, maintenanceKomponenPath, komponenPath, detailMisiPath, detailOperatorPath, detailWahanaPath, detailMaintenanceWahanaPath, detailMaintenanceKomponenPath, detailKomponenPath, loginPath, profilePath } from "./routes";
 
 function App() {
   const location = useLocation();
-  const showSideBar = ![loginPath].includes(location.pathname);
+  const showSidebar = ![loginPath].includes(location.pathname);
 
   return (
     <div className="app">
-      {showSideBar && <SideBar />}
+      {showSidebar && <Sidebar />}
       <main>
         <Routes>
           <Route path={loginPath} element={<LoginPage />} />
@@ -33,12 +35,14 @@ function App() {
           <Route path={misiPath} element={<ProtectedRoute element={MisiPage} />} />
           <Route path={operatorPath} element={<ProtectedRoute element={OperatorPage} />} />
           <Route path={wahanaPath} element={<ProtectedRoute element={WahanaPage} />} />
-          <Route path={maintenancePath} element={<ProtectedRoute element={MaintenancePage} />} />
+          <Route path={maintenanceWahanaPath} element={<ProtectedRoute element={MaintenanceWahanaPage} />} />
+          <Route path={maintenanceKomponenPath} element={<ProtectedRoute element={MaintenanceKomponenPage} />} />
           <Route path={komponenPath} element={<ProtectedRoute element={KomponenPage} />} />
           <Route path={detailMisiPath} element={<ProtectedRoute element={DetailMisiPage} />} />
-          <Route path={detailOperatorPath} element={<ProtectedRoute element={DetailOperatorPage} />} />
-          <Route path={detailWahanaPath} element={<ProtectedRoute element={DetailWahanaPage} />} />
-          <Route path={detailMaintenancePath} element={<ProtectedRoute element={DetailMaintenancePage} />} />
+          <Route path={`${detailOperatorPath}/:uuid`} element={<ProtectedRoute element={DetailOperatorPage} />} />
+          <Route path={`${detailWahanaPath}/:uuid`} element={<ProtectedRoute element={DetailWahanaPage} />} />
+          <Route path={detailMaintenanceWahanaPath} element={<ProtectedRoute element={DetailMaintenanceWahanaPage} />} />
+          <Route path={detailMaintenanceKomponenPath} element={<ProtectedRoute element={DetailMaintenanceKomponenPage} />} />
           <Route path={detailKomponenPath} element={<ProtectedRoute element={DetailKomponenPage} />} />
         </Routes>
       </main>
