6fed35f4c355b10341e64a463d27fc0d7bcb312c
rendypratama01 <m.rendy5pratama@gmail.com>
2024-08-16 21:11:45 +0700

    implementasi api halaman komponen

--- src/components/DetailKomponen.js
+++ src/components/DetailKomponen.js
@@ -1,106 +1,105 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Tab from "react-bootstrap/Tab";
 import Tabs from "react-bootstrap/Tabs";
-import image from "../assets/img/image.png";
+import { useParams } from "react-router-dom";
+import komponenService from "../services/komponen.service"; // Sesuaikan path import
+import Zoom from "react-medium-image-zoom";
+import "react-medium-image-zoom/dist/styles.css";
 
 const DetailKomponen = () => {
-  const komponeneDetails = {
-    idKomponen: "COMP-001",
-    harga: "Rp. 200.000",
-    toko: "UAV Indo",
-    tanggalBeli: "1 Mei 2024",
-    operasional:"10 Jam",
-  };
+  const { uuid } = useParams(); // Get the uuid from URL parameters
+  const [komponenDetails, setKomponenDetails] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-  const misi = [
-    { id: 1, judul: 'Pemetaan Krakatau', kategori: 'Pemetaan', tanggal: '15 Agustus 2023' },
-    { id: 2, judul: 'Monitoring Gunung Bromo', kategori: 'Monitoring', tanggal: '26 Agustus 2023' },
-    { id: 3, judul: 'Kompetisi Fotografi Semeru', kategori: 'Kompetisi', tanggal: '13 September 2023' },
-    { id: 4, judul: 'Pengiriman Logistik Merapi', kategori: 'Pengiriman', tanggal: '19 Oktober 2023' },
-    { id: 5, judul: 'Pemetaan Rinjani', kategori: 'Pemetaan', tanggal: '14 November 2023' },
-    { id: 6, judul: 'Monitoring Gunung Agung', kategori: 'Monitoring', tanggal: '15 Desember 2023' }
-  ];
+  useEffect(() => {
+    const fetchWahanaDetails = async () => {
+      try {
+        const data = await komponenService.getKomponenById(uuid);
+        console.log(data); // Logging data for debugging
+        setKomponenDetails(data.komponen);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
 
-  const perbaikan = [
-    { id: 1, judul: 'Perbaikan Struktur Rangka UAV', kategori: 'Airframe', tanggal: '10 Januari 2023' },
-    { id: 2, judul: 'Upgrade Sistem Navigasi UAV', kategori: 'Elektronik', tanggal: '15 Februari 2023' },
-    { id: 3, judul: 'Penggantian Baterai UAV', kategori: 'Elektronik', tanggal: '20 Maret 2023' },
-    { id: 4, judul: 'Optimisasi Sistem Kamera UAV', kategori: 'Elektronik', tanggal: '12 Mei 2023' },
-    { id: 5, judul: 'Perbaikan Sayap UAV', kategori: 'Airframe', tanggal: '25 Juni 2023' },
-    { id: 6, judul: 'Perbaikan Motor UAV', kategori: 'Elektronik', tanggal: '18 Agustus 2023' }
-  ];
+    fetchWahanaDetails();
+  }, [uuid]);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error}</p>;
+  if (!komponenDetails) return <p>No Komponen details available.</p>;
 
   return (
-    <div className="absolute ml-cl7 mr-cr1 mt-ct1">
-      <h3 className="text-3xl text-new-300">Komponen</h3>
+    <div className="ml-cl7 mr-cr1">
+      <h3 className="text-3xl text-new-300 pt-10">{komponenDetails.nama_komponen}</h3>
       <p className="text-justify">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum.
+        {komponenDetails.deskripsi_komponen}
       </p>
-      <div className="flex justify-center items-center mt-ct1 mb-cb1"> 
-        <img src={image} alt="Logo Detail Misi" width="400px" />
+      <div className="flex justify-center items-center mt-ct1 mb-cb1">
+        {komponenDetails.foto_komponens.map((foto, index) => (
+          <Zoom key={index}>
+            <img src={foto.foto_urls} alt={`Komponen ${index + 1}`} width="300px" />
+          </Zoom>
+        ))}
       </div>
       <div className="detail_maintenance-container-sub_title">
         <Tabs defaultActiveKey="detail" id="tab" className="mb-3">
           <Tab eventKey="detail" title="Detail">
-          <div className="mt-3">
+            <div className="mt-3">
               <div className="flex mb-2">
                 <span className="font-bold w-40">ID Komponen</span>
                 <span className="w-1 mx-1">:</span>
-                <span className="flex-1 text-left">{komponeneDetails.idKomponen}</span>
+                <span className="flex-1 text-left">{komponenDetails.id_komponen}</span>
               </div>
               <div className="flex mb-2">
                 <span className="font-bold w-40">Harga</span>
                 <span className="w-1 mx-1">:</span>
-                <span className="flex-1 text-left">{komponeneDetails.harga}</span>
+                <span className="flex-1 text-left">{komponenDetails.harga}</span>
               </div>
               <div className="flex mb-2">
                 <span className="font-bold w-40">Toko</span>
                 <span className="w-1 mx-1">:</span>
-                <span className="flex-1 text-left">{komponeneDetails.toko}</span>
+                <span className="flex-1 text-left">{komponenDetails.tempat_pembelian}</span>
               </div>
               <div className="flex mb-2">
                 <span className="font-bold w-40">Tanggal Beli</span>
                 <span className="w-1 mx-1">:</span>
-                <span className="flex-1 text-left">{komponeneDetails.tanggalBeli}</span>
+                <span className="flex-1 text-left">{komponenDetails.createdAt}</span>
               </div>
               <div className="flex mb-2">
                 <span className="font-bold w-40">Operasional</span>
                 <span className="w-1 mx-1">:</span>
-                <span className="flex-1 text-left">{komponeneDetails.operasional}</span>
+                <span className="flex-1 text-left">{komponenDetails.operasional}</span>
               </div>
-            </div>      
+            </div>
           </Tab>
           <Tab eventKey="misi" title="Misi">
-          <div className="mt-3 grid grid-cols-1 gap-6">
-              {misi.map(misi => (
+            <div className="mt-3 grid grid-cols-1 gap-6">
+            {komponenDetails.missions.map((mission) => (
                 <div
-                  key={misi.id}
+                  key={mission.id}
                   className="bg-white border border-gray-300 rounded-lg p-4 hover:bg-gray-100 cursor-pointer"
                 >
-                  <h4 className="text-lg font-semibold text-gray-800">{misi.judul}</h4>
-                  <p className="text-gray-600">{misi.kategori}</p>
-                  <p className="text-gray-600">{misi.tanggal}</p>
+                  <h4 className="text-lg font-semibold text-gray-800">{mission.judul_misi}</h4>
+                  <p className="text-gray-600">kategori: {mission.kategori}</p>
+                  <p className="text-gray-600">Tanggal: {mission.createdAt}</p>
                 </div>
               ))}
             </div>
           </Tab>
-
           <Tab eventKey="perbaikan" title="Perbaikan">
-          <div className="mt-3 grid grid-cols-1 gap-6">
-              {perbaikan.map(perbaikan => (
+            <div className="mt-3 grid grid-cols-1 gap-6">
+            {komponenDetails.komponen_perbaikanKomponen.map((perbaikan) => (
                 <div
                   key={perbaikan.id}
                   className="bg-white border border-gray-300 rounded-lg p-4 hover:bg-gray-100 cursor-pointer"
                 >
-                  <h4 className="text-lg font-semibold text-gray-800">{perbaikan.judul}</h4>
-                  <p className="text-gray-600">{perbaikan.kategori}</p>
-                  <p className="text-gray-600">{perbaikan.tanggal}</p>
+                  <h4 className="text-lg font-semibold text-gray-800">{perbaikan.judul_perbaikan}</h4>
+                  <p className="text-gray-600">kategori: {perbaikan.kategori}</p>
+                  <p className="text-gray-600">Tanggal: {perbaikan.biaya}</p>
                 </div>
               ))}
             </div>
@@ -108,7 +107,8 @@ const DetailKomponen = () => {
         </Tabs>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default DetailKomponen
+export default DetailKomponen;
+
--- src/components/Komponen.js
+++ src/components/Komponen.js
@@ -1,35 +1,42 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import DataTable from "react-data-table-component";
 import { FaEdit, FaPlus } from "react-icons/fa";
 import { MdDelete } from "react-icons/md";
 import { TbListDetails } from "react-icons/tb";
-import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
-import Form from 'react-bootstrap/Form';
-import { detailKomponenPath } from '../routes';
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import Form from "react-bootstrap/Form";
+import { detailKomponenPath } from "../routes";
 import komponenService from "../services/komponen.service";
+import ReactDOM from "react-dom";
 
 function Komponen() {
   const [showModal, setShowModal] = useState(false);
+  const [isUpdate, setIsUpdate] = useState(false); // State for edit mode
   const [formData, setFormData] = useState({
-    nama: '',
-    deskripsi: '',
-    idkomponen: '',
-    harga: '',
-    toko: '',
-    operasional: '',
-    tanggalBeli: '',
-    foto: null
+    nama: "",
+    deskripsi: "",
+    kategori: "",
+    tempat_pembelian: "",
+    harga: "",
+    photos: [],
   });
   const [komponenData, setKomponenData] = useState([]);
+  const [searchQuery, setSearchQuery] = useState(""); // State for search query
+  const [photoError, setPhotoError] = useState("");
+  const [komponenId, setKomponenId] = useState(null);
+  const [showConfirmDelete, setShowConfirmDelete] = useState(false);
+  const [showSuccessDelete, setShowSuccessDelete] = useState(false);
+  const [deleteKomponenId, setDeleteKomponenId] = useState(null);
+  const [showSuccessAdd, setShowSuccessAdd] = useState(false);
+  const [showSuccessUpdate, setShowSuccessUpdate] = useState(false);
 
   useEffect(() => {
     const fetchKomponen = async () => {
       try {
         const response = await komponenService.getKomponen();
-        console.log("Fetched Komponen Response:", response);
+        console.log(response?.msg, response);
 
-        // Cek jika respons adalah objek dan memiliki properti 'Komponen'
         if (
           typeof response === "object" &&
           response !== null &&
@@ -43,38 +50,126 @@ function Komponen() {
           );
         }
       } catch (error) {
-        console.error("Error fetching wahana data:", error);
+        console.error(error.msg, error);
       }
     };
 
     fetchKomponen();
   }, []);
 
+  useEffect(() => {
+    const searchKomponenData = async () => {
+      try {
+        if (searchQuery) {
+          const response = await komponenService.searchKomponen({
+            query: searchQuery,
+          });
+          if (response && Array.isArray(response.komponen)) {
+            setKomponenData(response.komponen);
+          } else {
+            console.error("Invalid search response format");
+          }
+        } else {
+          const response = await komponenService.getKomponen();
+          if (response && Array.isArray(response.komponen)) {
+            setKomponenData(response.komponen);
+          } else {
+            console.error("Invalid response format");
+          }
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    searchKomponenData();
+  }, [searchQuery]);
+
+  useEffect(() => {
+    if (komponenId) {
+      const fetchKomponenById = async () => {
+        try {
+          const response = await komponenService.getKomponenById(komponenId);
+          if (
+            typeof response === "object" &&
+            response !== null &&
+            response.komponen
+          ) {
+            setFormData({
+              nama: response.komponen.nama_komponen,
+              deskripsi: response.komponen.deskripsi_komponen,
+              kategori: response.komponen.kategori,
+              tempat_pembelian: response.komponen.tempat_pembelian,
+              harga: response.komponen.harga,
+              photos: response.komponen.photos || [],
+            });
+            handleShow();
+          } else {
+            console.error(
+              "Fetched data is not an object or does not contain 'komponen'."
+            );
+          }
+        } catch (error) {
+          console.error(error.msg, error);
+        }
+      };
+
+      fetchKomponenById();
+    }
+  }, [komponenId]);
+
   const handleShow = () => setShowModal(true);
-  const handleClose = () => setShowModal(false);
+  const handleClose = () => {
+    setShowModal(false);
+    setIsUpdate(false);
+    setKomponenId(null);
+    setFormData({
+      nama: "",
+      deskripsi: "",
+      kategori: "",
+      tempat_pembelian: "",
+      harga: "",
+      photos: [],
+    });
+    setPhotoError("");
+  };
+
+  const handleDeletePhoto = (index) => {
+    const updatedPhotos = formData.photos.filter((_, i) => i !== index);
+    setFormData({
+      ...formData,
+      photos: updatedPhotos,
+    });
+  };
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
     setFormData({
       ...formData,
-      [name]: value
+      [name]: value,
     });
   };
 
   const handleFileChange = (e) => {
-    setFormData({
-      ...formData,
-      foto: e.target.files[0]
-    });
+    const { files } = e.target;
+    const selectedFiles = Array.from(files);
+
+    if (selectedFiles.length + formData.photos.length > 1) {
+      setPhotoError("You can upload a maximum of 1 photos.");
+    } else {
+      setFormData({
+        ...formData,
+        photos: [...formData.photos, ...selectedFiles],
+      });
+      setPhotoError("");
+    }
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    // Create a FormData instance
     const dataToSubmit = new FormData();
 
-    // Append form data fields
     Object.keys(formData).forEach((key) => {
       if (Array.isArray(formData[key])) {
         formData[key].forEach((file) => dataToSubmit.append(key, file));
@@ -84,14 +179,88 @@ function Komponen() {
     });
 
     try {
-      const response = await komponenService.addKomponen(dataToSubmit);
-      console.log("Komponen data successfully posted:", response);
-      handleClose(); // Close the modal
-      // Optionally, show a success message or update the data table
+      if (isUpdate) {
+        const response = await komponenService.updateKomponen(
+          komponenId,
+          dataToSubmit
+        );
+        console.log(response.msg, response);
+
+        // Display success update modal
+        setShowSuccessUpdate(true);
+      } else {
+        const response = await komponenService.addKomponen(dataToSubmit);
+        console.log(response.msg, response);
+
+        // Display success add modal
+        setShowSuccessAdd(true);
+      }
+
+      const updatedResponse = await komponenService.getKomponen();
+      if (
+        typeof updatedResponse === "object" &&
+        updatedResponse !== null &&
+        Array.isArray(updatedResponse.komponen)
+      ) {
+        setKomponenData(updatedResponse.komponen);
+        console.log("Data successfully updated");
+      } else {
+        console.error(
+          "Fetched data is not an object or does not contain an array 'komponen'."
+        );
+      }
+
+      handleClose();
     } catch (error) {
-      console.error("Error posting komponen data:", error);
-      // Optionally, show an error message to the user
+      console.error(
+        isUpdate
+          ? "Error updating komponen data:"
+          : "Error posting komponen data:",
+        error
+      );
     }
+  };
+
+  const handleDeleteConfirm = async () => {
+    try {
+      await komponenService.deleteKomponen(deleteKomponenId);
+      setShowSuccessDelete(true);
+
+      const updatedResponse = await komponenService.getKomponen();
+      if (
+        typeof updatedResponse === "object" &&
+        updatedResponse !== null &&
+        Array.isArray(updatedResponse.komponen)
+      ) {
+        setKomponenData(updatedResponse.komponen);
+      } else {
+        console.error(
+          "Fetched data is not an object or does not contain an array 'Komponen'."
+        );
+      }
+
+      setShowConfirmDelete(false);
+    } catch (error) {
+      console.error("Error deleting komponen:", error);
+    }
+  };
+
+  const handleEdit = (komponen) => {
+    setIsUpdate(true);
+    setKomponenId(komponen.uuid);
+  };
+
+  const handleDelete = (uuid) => {
+    setDeleteKomponenId(uuid);
+    setShowConfirmDelete(true);
+  };
+
+  const customStyles = {
+    headCells: {
+      style: {
+        backgroundColor: "#EAEAEA", // Change to your desired color
+      },
+    },
   };
 
   const columns = [
@@ -101,18 +270,13 @@ function Komponen() {
       sortable: true,
     },
     {
-      name: "Deskripsi",
-      selector: (row) => row.deskripsi_komponen,
+      name: "Kategori",
+      selector: (row) => row.kategori,
       sortable: true,
     },
     {
       name: "ID Komponen",
-      selector: (row) => row.idkomponen,
-      sortable: true,
-    },
-    {
-      name: "Kategori",
-      selector: (row) => row.kategori,
+      selector: (row) => row.id_komponen,
       sortable: true,
     },
     {
@@ -120,12 +284,22 @@ function Komponen() {
       cell: (row) => (
         <div className="flex gap-3">
           <button className="text-gray-700">
-            <a href={detailKomponenPath} className="no-underline hover:no-underline text-inherit">
-              <TbListDetails className="text-2xl"/>
+            <a
+              href={`${detailKomponenPath}/${row.uuid}`} // Adjust path as needed
+              className="no-underline hover:no-underline text-inherit"
+            >
+              <TbListDetails className="text-2xl" />
             </a>
           </button>
-          <button className="text-blue-900"><FaEdit className="text-2xl"/></button>
-          <button className="text-red-600"><MdDelete className="text-2xl"/></button>
+          <button className="text-blue-900" onClick={() => handleEdit(row)}>
+            <FaEdit className="text-2xl" />
+          </button>
+          <button
+            className="text-red-600"
+            onClick={() => handleDelete(row.uuid)} // Add this line
+          >
+            <MdDelete className="text-2xl" />
+          </button>
         </div>
       ),
     },
@@ -134,28 +308,45 @@ function Komponen() {
   return (
     <div className="ml-cl7">
       <h3 className="pt-10 text-3xl text-new-300">Komponen</h3>
+
+      {/* Search Bar */}
+      <div className="mb-4 pt-5">
+        <Form inline>
+          <Form.Control
+            type="text"
+            placeholder="Cari....."
+            className="mr-2"
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+          />
+        </Form>
+      </div>
+
       <DataTable
         title="Data Komponen"
         columns={columns}
         data={komponenData}
-        fixedHeader 
+        fixedHeader
         fixedHeaderScrollHeight="530px"
         pagination
         highlightOnHover
+        customStyles={customStyles}
         actions={
           <Button
-            style={{ backgroundColor: '#5A6ACF', color: '#ffffff' }}
+            style={{ backgroundColor: "#5A6ACF", color: "#ffffff" }}
             className="d-flex align-items-center"
             onClick={handleShow}
           >
-            Tambah <FaPlus className="text-sm"/>
+            Tambah <FaPlus className="text-sm" />
           </Button>
         }
       />
-      
+
       <Modal show={showModal} onHide={handleClose}>
         <Modal.Header closeButton>
-          <Modal.Title>Tambah Komponen</Modal.Title>
+          <Modal.Title>
+            {isUpdate ? "Edit Komponen" : "Tambah Komponen"}
+          </Modal.Title>
         </Modal.Header>
         <Modal.Body>
           <h5>Informasi</h5>
@@ -182,15 +373,18 @@ function Komponen() {
               />
             </Form.Group>
 
-            <Form.Group controlId="formIdKomponen" className="mt-3">
-              <Form.Label>ID Komponen</Form.Label>
+            <Form.Group controlId="formKategori" className="mt-3">
+              <Form.Label>Kategori</Form.Label>
               <Form.Control
-                type="text"
-                placeholder="Enter ID Komponen"
-                name="idkomponen"
-                value={formData.idkomponen}
+                as="select"
+                name="kategori"
+                value={formData.kategori}
                 onChange={handleInputChange}
-              />
+              >
+              <option value="">Pilih kategori...</option>
+              <option value="Kamera">Kamera</option>
+              <option value="Sensor">Sensor</option>
+              </Form.Control>
             </Form.Group>
 
             <Form.Group controlId="formHarga" className="mt-3">
@@ -209,49 +403,147 @@ function Komponen() {
               <Form.Control
                 type="text"
                 placeholder="Enter Toko Komponen"
-                name="toko"
-                value={formData.toko}
-                onChange={handleInputChange}
-              />
-            </Form.Group>
-
-            <Form.Group controlId="formOperasional" className="mt-3">
-              <Form.Label>Operasional (Rp)</Form.Label>
-              <Form.Control
-                type="number"
-                placeholder="Enter Operasional Komponen"
-                name="operasional"
-                value={formData.operasional}
-                onChange={handleInputChange}
-              />
-            </Form.Group>
-
-            <Form.Group controlId="formTanggalBeli" className="mt-3">
-              <Form.Label>Tanggal Beli</Form.Label>
-              <Form.Control
-                type="date"
-                placeholder="Enter Tanggal Beli Komponen"
-                name="tanggalBeli"
-                value={formData.tanggalBeli}
+                name="tempat_pembelian"
+                value={formData.tempat_pembelian}
                 onChange={handleInputChange}
               />
             </Form.Group>
 
             <Form.Group controlId="formFoto" className="mt-3">
-              <Form.Label>Upload Foto</Form.Label>
+              <Form.Label>Upload Foto komponen</Form.Label>
               <Form.Control
                 type="file"
-                name="foto"
+                name="photos"
+                multiple
                 onChange={handleFileChange}
               />
+              {photoError && (
+                <div className="text-danger mt-2">{photoError}</div>
+              )}
             </Form.Group>
 
+            <div className="mt-3">
+              {formData.photos && formData.photos.length > 0 && (
+                <div className="mt-4">
+                  {formData.photos.map((file, index) => (
+                    <div key={index} className="mt-4 d-flex align-items-center">
+                      <img
+                        src={URL.createObjectURL(file)}
+                        alt={`Foto ${index}`}
+                        className="img-fluid"
+                        style={{ maxWidth: "350px", margin: "10px" }}
+                      />
+                      <Button
+                        variant="danger"
+                        onClick={() => handleDeletePhoto(index)}
+                        className="ms-2"
+                      >
+                        Hapus
+                      </Button>
+                    </div>
+                  ))}
+                </div>
+              )}
+            </div>
+
             <Button variant="primary" type="submit" className="mt-4">
-              Submit
+              {isUpdate ? "Update" : "Submit"}
             </Button>
           </Form>
         </Modal.Body>
       </Modal>
+
+      {ReactDOM.createPortal(
+        showConfirmDelete && (
+          <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
+            <div className="bg-white p-8 rounded-lg shadow-lg w-96">
+              <h2 className="text-lg font-semibold mb-4">Konfirmasi Hapus</h2>
+              <p className="mb-4">
+                Apakah Anda yakin ingin menghapus data komponen ini?
+              </p>
+              <div className="flex justify-end gap-4">
+                <button
+                  className="bg-gray-300 text-gray-700 px-4 py-2 rounded hover:bg-gray-400"
+                  onClick={() => setShowConfirmDelete(false)}
+                >
+                  Batal
+                </button>
+                <button
+                  className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600"
+                  onClick={handleDeleteConfirm}
+                >
+                  Hapus
+                </button>
+              </div>
+            </div>
+          </div>
+        ),
+        document.body
+      )}
+
+      {ReactDOM.createPortal(
+        showSuccessDelete && (
+          <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
+            <div className="bg-white p-8 rounded-lg shadow-lg w-96">
+              <h2 className="text-lg font-semibold mb-4">Berhasil Dihapus</h2>
+              <p className="mb-4">komponen telah berhasil dihapus.</p>
+              <div className="flex justify-end gap-4">
+                <button
+                  className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700"
+                  onClick={() => setShowSuccessDelete(false)}
+                >
+                  Oke
+                </button>
+              </div>
+            </div>
+          </div>
+        ),
+        document.body
+      )}
+
+      {ReactDOM.createPortal(
+        showSuccessAdd && (
+          <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
+            <div className="bg-white p-8 rounded-lg shadow-lg w-96">
+              <h2 className="text-lg font-semibold mb-4">
+                Berhasil Tambah Data
+              </h2>
+              <p className="mb-4">Data komponen telah berhasil ditambahkan.</p>
+              <div className="flex justify-end gap-4">
+                <button
+                  className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700"
+                  onClick={() => setShowSuccessAdd(false)}
+                >
+                  Oke
+                </button>
+              </div>
+            </div>
+          </div>
+        ),
+        document.body
+      )}
+
+      {ReactDOM.createPortal(
+        showSuccessUpdate && (
+          <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
+            <div className="bg-white p-8 rounded-lg shadow-lg w-96">
+              <h2 className="text-lg font-semibold mb-4">
+                Berhasil Update Data
+              </h2>
+              <p className="mb-4">Data komponen telah berhasil diperbarui.</p>
+              <div className="flex justify-end gap-4">
+                <button
+                  className="bg-blue-400 text-white px-4 py-2 rounded hover:bg-blue-700"
+                  onClick={() => setShowSuccessUpdate(false)}
+                >
+                  Oke
+                </button>
+              </div>
+            </div>
+          </div>
+        ),
+        document.body
+      )}
     </div>
   );
 }
